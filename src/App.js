a11facef93482da96c806ba84086a571d234319e
ebi10sa22 <ebenezerebi500@gmail.com>
2021-01-13 15:06:09 +0530

    Comment provided

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import "./App.css"
 import Tables from "./Components/Tables"
 function App() {
+
+  // Content for price table
   const titles = ["STARTER", "PROFESSIONAL", "ORGANIZATION"]
   const features = ["Centralized teams",
     "Version history", "Plugins administration", "Facebood & Instagram ads", "Design system analysis", "Unlimited cloud storage"
   ]
   const display = { Starter: [true, true, true, false, false, false, false], Professional: [true, true, true, true, false, false], Organization: [true, true, true, true, true, true] }
   const prices = [0, 25, 50]
+
+
   return (
     <div className="app">
+      {/* Tables Component for rendering the space for talbes */}
       <Tables titles={titles[0]} features={features} display={display.Starter} prices={prices[0]} background={false} />
       <Tables titles={titles[1]} features={features} display={display.Professional} prices={prices[1]} background={true} />
       <Tables titles={titles[2]} features={features} display={display.Organization} prices={prices[2]} background={false} />
--- src/Components/Tables.js
+++ src/Components/Tables.js
@@ -6,8 +6,13 @@ import "./Tables.css"
 function Tables({ background, prices, titles, features, display }) {
   return (
     <div className="table" style={{ background: !background && "none", zIndex: !background && "0", boxShadow: !background && "none", color: background && "white" }}>
+      {/* header contains title and price details*/}
       <Header background={background} titles={titles} prices={prices} />
+
+      {/* Main component (i.e) body of the table contains the features specified */}
       <Main features={features} display={display} />
+
+      {/* Footer component contains a button */}
       <Footer background={background} display={display} titles={titles} />
     </div>
   )
